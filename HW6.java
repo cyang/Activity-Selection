5244c65cff475ea32e36a93ac359ea7beb77a5e8
Christopher Yang <cyang001@citymail.cuny.edu>
2015-05-11 15:18:43 -0400

    Correct

--- HW6.java
+++ HW6.java
@@ -4,31 +4,53 @@
 public abstract class HW6 {
     public static void main(String[] args) {
 
-        int[] s = {1, 2, 4, 1, 5, 8, 9, 11, 13};
-        int[] f = {3, 5, 7, 8, 9, 10, 11, 14, 16};
-    }
+        int[] s = {0, 1, 2, 4, 1, 5, 8, 9, 11, 13};
+        int[] f = {0, 3, 5, 7, 8, 9, 10, 11, 14, 16};
 
-    public static int[][] getTable(int[] s, int[] f) {
-
-        int n = s.length;
-
-        int[][] table = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j)
-                    table[i][j] = 1;
-                else
-                    table[i][j] = maxActivity(s, f, i, j);
-            }
+        int[] table = new int[s.length];
+        for (int i = 0; i < s.length; i++) {
+            if (i == 0)
+                table[i] = 0;
+            else
+                table[i] = -1;
         }
 
-        return table;
+        activitySelection(s, f, table);
+
+
+        for(int element: table){
+            System.out.print(element + " ");
+        }
+    }
+
+    public static void activitySelection(int[] s, int[] f, int[] table){
+
+
+        for(int i = 1; i < table.length; i++){
+            if(table[i] == -1) {
+                int j = i;
+
+                while(f[j-1] > s[i])
+                    --j;
+
+                int nonOverlap = j-1;
+                table[i] = max(table[i-1], table[nonOverlap] + 1);
+            }
+
+
+        }
+
+
+
     }
 
 
-    public static int maxActivity(int[] s, int[] f, int i, int j){
-        if(s[])
-
+    public static int max(int a, int b){
+        if(a > b)
+            return a;
+        else
+            return b;
     }
+
 }
 
